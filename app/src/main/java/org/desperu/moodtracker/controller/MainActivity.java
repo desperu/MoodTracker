44865e3a92188c83eb094e8141fa6960bd263f95
desperu <motais.esteban@laposte.net>
2019-07-27 15:28:51 +0200

    Perfect compareTime method for mood age text, and re-use compareDate for mood cycle

--- app/src/main/java/org/desperu/moodtracker/controller/MainActivity.java
+++ app/src/main/java/org/desperu/moodtracker/controller/MainActivity.java
@@ -158,7 +158,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        if (moodUtils.compareTime(moodUtils.getLongPrefs(MainActivity.this,
+                        if (moodUtils.compareDate(moodUtils.getLongPrefs(MainActivity.this,
                                 moodDayFile, currentDate)) >= 0)
                             goodDate = true;
                         else { // if the date is again lower, restart this dialog.
@@ -189,7 +189,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     protected void onResume() {
         goodDate = true;
         // Get current date and last saved date difference.
-        int checkDate = moodUtils.compareTime(moodUtils.getLongPrefs(this, moodDayFile, currentDate));
+        int checkDate = moodUtils.compareDate(moodUtils.getLongPrefs(this, moodDayFile, currentDate));
         if (checkDate < 0 && wrongDateEnabled) {
             // If current date is lower and wrongDateEnabled, show wrongDateDialog.
             goodDate = false;
